70cf46becd62f0db79586bc29d7b624fb310efed
Andrew Wilson <andrew.wilson@blue-zinc.com>
2023-04-19 13:03:45 +0100

    new info

---
+++ pythonProject/pythonProject/test.py
@@ -0,0 +1,18 @@
+l = 6
+
+x = input('Write anything here: ')
+
+while l > 1:
+   x = input('Write anything here: ')
+   if x == 'cool':
+      print('thats damn impressive son, but youre done')
+      break
+   print(x)
+   continue
+
+
+
+
+
+
+
